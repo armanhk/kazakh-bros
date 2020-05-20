3c7cce895a6a5d49877d6df90d98ecdf5a5ff3e1
Arman K <arman.ucsd@gmail.com>
2020-05-19 18:12:42 -0700

    Started prototyping a 2d shooter - player movement and bounds done

---
+++ Assets/Scripts/Shooter Prototype/PlayerController.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PlayerController : MonoBehaviour
+{
+    private SceneController sceneController;
+    private float objectWidth;
+
+    private void Awake()
+    {
+        sceneController = FindObjectOfType<SceneController>();
+        objectWidth = transform.GetComponent<SpriteRenderer>().bounds.size.x;
+    }
+
+    private void Update()
+    {
+        MovePlayer();
+    }
+
+    private void MovePlayer()
+    {
+        // Define the movement
+        float playerDirection = Input.GetAxis("Horizontal");
+        Vector3 playerPosition = transform.position;
+        float horizontalMovement = playerDirection * Time.deltaTime * sceneController.playerSpeed;
+        Vector2 playerMovement = new Vector2(horizontalMovement, 0);
+
+        if (playerPosition.x + playerMovement.x > -sceneController.screenBounds.x + objectWidth/2
+            && playerPosition.x + playerMovement.x < sceneController.screenBounds.x - objectWidth/2)
+        {
+            transform.Translate(playerMovement);
+        }
+    }
+}
---
+++ Assets/Scripts/Shooter Prototype/SceneController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SceneController : MonoBehaviour
+{
+    public float playerSpeed;
+    public Vector2 screenBounds;
+
+    private void Awake()
+    {
+        screenBounds = Camera.main.ScreenToWorldPoint(new Vector3(Screen.width, Screen.height, Camera.main.transform.position.z));
+    }
+
+    // Start is called before the first frame update
+    private void Start()
+    {
+        playerSpeed = 10;
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+        
+    }
+}
